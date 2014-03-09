966c8966d20c3e1d2a249347ff55cfd23313bbfd
Adam Smith <adams@spotify.com>
2014-03-09 15:26:58 -0400

    Tidied things
    -Removed normalize funtion from server.js
    +Required utils in server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-// socket.io way
 var j5 = require('johnny-five'),
   express = require('express'),
   app = express(),
   server = require('http').createServer(app),
   io = require('socket.io').listen(server),
+  utils = require('./js/utils'),
   port = process.env.PORT || 4567;
 
-var controllerAttached = false;
+var isArduinoConnected = false;
 
 //johnny-five thingies
 var potFilter, potQ;
@@ -29,7 +29,7 @@ io.sockets.on('connection', function (socket) {
   socket.on('app:loaded', function() {
 
     // manage johnny-five
-    if (controllerAttached) {
+    if (isArduinoConnected) {
       j5.Board().on('ready', function() {
         // 100 = max range value of gain html range input
         var inputGainMax = 100;
@@ -49,8 +49,8 @@ io.sockets.on('connection', function (socket) {
         });
 
         potFilter.on('read', function() {
-          var calcValue = normalize(inputFilterMax, potMax, this.raw);
-          var knobValue = normalize(knobMax, potMax, this.raw);
+          var calcValue = utils.normalize(inputFilterMax, potMax, this.raw);
+          var knobValue = utils.normalize(knobMax, potMax, this.raw);
 
           socket.emit('j5:potFilter:read', {calculated: calcValue, knob: knobValue});
         });
@@ -62,25 +62,21 @@ io.sockets.on('connection', function (socket) {
         });
 
         potQ.on('read', function() {
-          // 1 = max range value of html input
-          // 270 = max rotation of the knob
-          // 1023 = max range value of potentiometer
-          var calcValue = normalize(inputFilterMax, potMax, this.raw);
-          var knobValue = normalize(knobMax, potMax, this.raw);
+          var calcValue = utils.normalize(inputFilterMax, potMax, this.raw);
+          var knobValue = utils.normalize(knobMax, potMax, this.raw);
 
           socket.emit('j5:potQ:read', {calculated: calcValue, knob: knobValue});
         });
 
+        // Gain
         potGain = new j5.Sensor({
           pin: 'A2',
           freq: 250
         });
 
         potGain.on('read', function() {
-          // 270 = max rotation of the knob
-          // 1023 = max range value of potentiometer
-          var calcValue = normalize(inputGainMax, potMax, this.raw);
-          var knobValue = normalize(knobMax, potMax, this.raw);
+          var calcValue = utils.normalize(inputGainMax, potMax, this.raw);
+          var knobValue = utils.normalize(knobMax, potMax, this.raw);
 
           socket.emit('j5:potGain:read', {calculated: calcValue, knob: knobValue});
         });
@@ -94,33 +90,9 @@ io.sockets.on('connection', function (socket) {
           socket.emit('j5:button1:down');
         });
 
-        //button 2
-        /*
-        button2 = new j5.Button({
-          pin: 3
-        });
-
-        button2.on('down', function() {
-          socket.emit('j5:button2:down');
-        });
-        */
       }); //end j5
     }
 
   });
 
 });
-
-/**
- * Normalize a given value from a larger range of numbers to a smaller
- * range of numbers.
- * Specifically, this is normalizing the pot range of 0 - 1023 to the html range input of 1 - 100
- *
- * @param scaleMax {number} The largest number in the range being scaled to
- * @param rangeMax {number} The largest number in the range the value appeared in
- * @param value {number} The number to be scaled
- * @return number
- */
-function normalize(scaleMax, rangeMax, value) {
-  return scaleMax * (value / rangeMax);
-}
