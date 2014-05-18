d2fc65261d692bc2b1e4ea9652c97eceb5202b61
Adam Smith <adamcsmith@gmail.com>
2014-05-17 20:26:26 -0400

    Step Sequencer CSS Updates
    + Renamed audiohack to jamnastik
    + Fixed up styles on stepsequencer
    + Added title to step sequencer instance

--- js/StepSequencer.js
+++ js/StepSequencer.js
@@ -3,7 +3,7 @@ var Pad = require('./Pad');
 /**
  * @constructor
  */
-function StepSequencer(id, context, pubsub, scheduler, socket) {
+function StepSequencer(id, context, pubsub, scheduler, socket, title) {
 
   /**
    * The step sequencer html element's id
@@ -78,6 +78,16 @@ function StepSequencer(id, context, pubsub, scheduler, socket) {
    * The grid's active row css class
    */
   this.rowActiveClass = 'active';
+
+  /**
+   * The displayed tabbed title
+   */
+  this.title = title;
+
+  /**
+   * Is this the active step sequencer?
+   */
+  this.isActive = true;
 }
 
 /**
@@ -86,9 +96,22 @@ function StepSequencer(id, context, pubsub, scheduler, socket) {
  */
 StepSequencer.prototype.init = function(samples) {
   this.samples = samples;
+  this._addTitle();
   this._setupGrid();
   this._handleEvents();
   this._handleIO();
+  return this;
+}
+
+/**
+ * Create title html element and append it to parent
+ * @return this
+ */
+StepSequencer.prototype._addTitle = function() {
+  var title = document.createElement('h2');
+  title.classList.add('title', 'step-sequencer-title', 'active');
+  title.innerText = this.title;
+  this.domEl.appendChild(title);
   return this;
 }
 
